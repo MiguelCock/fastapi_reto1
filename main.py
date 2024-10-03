1e50d1ed78ad838d20dd10235f5d592e6d3a0854
MiguelCock <macockc@eafit.edu.co>
2024-10-03 11:48:31 -0500

    finish the jsson file

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-
-class Event(BaseModel):
-    name: str
-    users: list = []
 
 
 class User(BaseModel):
@@ -12,19 +8,41 @@ class User(BaseModel):
     correo: str
 
 
+class Event(BaseModel):
+    name: str
+    users: list[User] = []
+
+
 event_list = []
+
+
+EVENTS_FILE = "events.json"
+
+
+def load_events():
+    global event_list
+    try:
+        with open(EVENTS_FILE, "r") as file:
+            events_data = json.load(file)
+            event_list = [Event(**event) for event in events_data]
+    except FileNotFoundError:
+        event_list = []
+
+
+def save_events():
+    with open(EVENTS_FILE, "w") as file:
+        json.dump([event.dict() for event in event_list], file, indent=4)
+
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+load_events()
 
 
 @app.post("/create_event")
 async def create_user(event: Event):
     event_list.append(event)
+    save_events()
     return {"message": "event created succesfully"}
 
 
@@ -39,5 +57,6 @@ async def subscribe(user: User, event_name: str):
     for event in event_list:
         if event.name == event_name:
             event.users.append(user)
+            save_events()
             return {"message": "user register succesfully"}
     return {"message": "user could not register"}
